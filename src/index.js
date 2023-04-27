5047d175a04f0f2aebfbd13d18e1a332435095b9
edgardsoza <120115282+edgardsoza@users.noreply.github.com>
2023-04-27 10:51:25 -0600

    try to publish

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter as Router } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import App from './App';
 import store from './store';
@@ -7,8 +8,10 @@ import store from './store';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <Router basename="/react-capstone-project">
     <Provider store={store}>
       <App />
     </Provider>
+    </Router>
   </React.StrictMode>,
 );
